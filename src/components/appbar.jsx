570a2ed4905a0b57ee3d33c5d07c29aea46b4134
Abdullah Mehmood Minhas <aminhas309@hotmail.com>
2023-06-28 03:31:29 +0500

    Cleaning Project

--- src/components/appbar.jsx
+++ src/components/appbar.jsx
@@ -4,38 +4,13 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
 import GitHubIcon from '@mui/icons-material/GitHub';
-import AdbIcon from '@mui/icons-material/Adb';
 
-const pages = [{name:"Home",link:"/"}, {name:"Traveling",link:"/travelling"}
-// , "Food", "Politics", "Weather", "Cars"
-];
 
 
 function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
 
   return (
     <AppBar
@@ -71,7 +46,9 @@ function ResponsiveAppBar() {
 
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open Repository">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <IconButton
+              href="https://github.com/minhas309/graph-task"
+              sx={{ p: 0 }}>
                 <GitHubIcon />
               </IconButton>
             </Tooltip>
